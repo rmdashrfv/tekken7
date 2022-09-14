4922e3b1ed584b462d5fc714c4fe8dc820c8c1a4
Michael Law <rmdashrfv@hey.com>
2022-09-14 16:27:35 -0400

    Added fighter elements

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ let fighters = ["heihachi", "kazuya", "bryan", "anna", "feng", "steve", "hwoaran
 const fighterUrl = (name) => {
   return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png` 
 }
+
+let fightersGrid = document.getElementById("fighters-grid")
+console.log("fighters grid", fightersGrid)
+
+// we have a list of fighters (the array)
+// we a function that builds urls for their images
+// now we need to iterate over the list of fighter names
+// create images
+// and add them to the grid
+
+fighters.forEach((fighter) => {
+  let div = document.createElement("div")
+  let img = document.createElement("img")
+  img.src = fighterUrl(fighter)
+  div.style.height = "100px"
+  div.style.width = "100px"
+  img.style.height = "100px"
+  img.style.width = "100px"
+  div.append(img)
+  fightersGrid.append(div)
+})
