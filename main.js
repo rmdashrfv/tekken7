20c36ed29c7b00fbbe04a92cd1b71e517c466fa3
Michael Law <rmdashrfv@hey.com>
2022-09-14 16:07:07 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let fighters = ["byran", "anna"]
+
+// Recreate the Fighters grid using this function
+
+const fighterUrl = (name) => {
+  return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png` 
+}
